6fc6fa4b735c8f7e457a7b52e272f6383c6c34fa
ʟᴇᴏ ᠰ ᴛɢ <ilashadam039@gmail.com>
2024-10-16 19:22:24 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,7 +10,7 @@ class Config:
     API_HASH = getenv("API_HASH", "")
     BOT_TOKEN = getenv("BOT_TOKEN", "")
     FSUB = getenv("FSUB", "ckautoapprovebot")
-    CHID = int(getenv("CHID", "-1002445847534"))
+    CHID = -1002445847534
     SUDO = list(map(int, getenv("SUDO", "7040444713 2144812475").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://hi:hi@auto.natzh.mongodb.net/?retryWrites=true&w=majority&appName=auto")
     
